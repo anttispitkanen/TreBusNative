9c21b731c21161b1965359a5449bc1689b638cbc
Antti Pitkänen <anttispitkanen@Anttis-MacBook-Pro.local>
2017-05-18 16:15:45 +0300

    adding navigation

--- src/Main.js
+++ src/Main.js
@@ -2,24 +2,24 @@ import React, { Component } from 'react';
 import { 
     ScrollView,
     View,
-    StyleSheet
+    StyleSheet,
+    Text,
+    Button
  } from 'react-native';
+
+import { StackNavigator, TabNavigator } from 'react-navigation';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Hotspots from './components/Hotspots';
+import Info from './components/Info';
 
-export default class Main extends Component {
-    render() {
-        return(
-            <ScrollView style={styles.main}>
-                <Header />
-                <Hotspots />
-                <Footer />
-            </ScrollView>
-        )
-    }
-}
+// export default class Main extends Component {
+    
+// }
+
+
+
 
 
 const styles = StyleSheet.create({
@@ -27,3 +27,89 @@ const styles = StyleSheet.create({
         flex: 1
     }
 })
+
+
+class MainScreen extends Component {
+    static navigationOptions = {
+        title: 'Home'
+    }
+    
+    render() {
+        const { navigate } = this.props.navigation;
+
+        return(
+            <ScrollView style={styles.main}>
+                <Header />
+                <Hotspots />
+                <Footer />
+                <Button
+                    onPress={() => navigate('Info')}
+                    title="Info"
+                />
+            </ScrollView>
+        )
+
+    }
+}
+
+const Main = StackNavigator({
+    Main: { screen: MainScreen },
+    Info: { screen: Info }
+})
+
+
+/*class HomeScreen extends Component {
+    static navigationOptions = {
+        title: 'Welcome'
+    }
+    render() {
+        const { navigate } = this.props.navigation;
+
+        return(
+            <View>
+                <Text>Hello, Navigation!</Text>
+                <Button 
+                    onPress={() => navigate('Chat', {user: 'Pötkö'})}
+                    title="Chat with someone"
+                />
+            </View>
+        ) 
+    }
+}
+
+
+class ChatScreen extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        title: `Chat with ${navigation.state.params.user}`
+    })
+    render() {
+        // The screen's current route is passed in to `props.navigation.state`
+        const { params } = this.props.navigation.state;
+        return(
+            <View>
+                <Text>Chat with {params.user}</Text>
+            </View>
+        )
+    }
+}
+
+class RecentScreen extends Component {
+    render() {
+        return <Text>Recent</Text>
+    }
+}
+
+class AllScreen extends Component {
+    render() {
+        return <Text>All</Text>
+    }
+}
+
+
+
+const Main = StackNavigator({
+    Home: { screen: HomeScreen },
+    Chat: { screen: ChatScreen }
+})*/
+
+export default Main;
---
+++ src/components/Info.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+export default class Info extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Inffoo :D</Text>
+            </View>
+        )
+    }
+}
